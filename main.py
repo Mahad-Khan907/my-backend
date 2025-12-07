caa974bc386a1ad05dc40d1a0bf65882a81cbf8d
Mahad Khan <kmahad902@gmail.com>
2025-12-08 01:45:53 +0500

    Change CORS settings to allow all origins

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", 
-        "https://physical-ai-bymahad.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
